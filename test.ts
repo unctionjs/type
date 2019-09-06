ea4307dd7a9d2584c442d09d5aba89744a19fc37
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-05 22:58:54 -0700

    Moving to jest and typing the test file

---
+++ test.ts
@@ -0,0 +1,34 @@
+/* eslint-disable no-undefined, no-magic-numbers */
+import type from "./index.ts";
+
+test(() => {
+  expect(type("a")).toEqual("String");
+});
+
+test(() => {
+  expect(type(1)).toEqual("Number");
+});
+
+test(() => {
+  expect(type({})).toEqual("Object");
+});
+
+test(() => {
+  expect(type([])).toEqual("Array");
+});
+
+test(() => {
+  expect(type(true)).toEqual("Boolean");
+});
+
+test(() => {
+  expect(type(false)).toEqual("Boolean");
+});
+
+test(() => {
+  expect(type(null)).toEqual("null");
+});
+
+test(() => {
+  expect(type(undefined)).toEqual("undefined");
+});
